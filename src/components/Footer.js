a535f9f102d705535156cf53d7b079ea671471e6
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-07-18 14:03:24 +0200

    balise paragraphe

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-        Boo Hamster géant de l'espace miniature &copy; - {new Date().getFullYear()}
+        <p>Boo Hamster géant de l'espace miniature &copy; - {new Date().getFullYear()}</p>
     </footer>
   );
 }
