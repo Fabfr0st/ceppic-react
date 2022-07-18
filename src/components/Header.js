defc549b4457a6063308866853ec6b124efce86d
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-07-18 14:06:18 +0200

    typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import "./Header.css"
 
 export default function Header({logo}) {
-  const minscDesc = "Rôdeur le plus connu de Faerûn";
+  const MinscDesc = "Rôdeur le plus connu de Faerûn";
 
   return (
     <header>
-      <img src={logo} alt={minscDesc}></img>
+      <img src={logo} alt={MinscDesc}></img>
       <h1>Rôdeur le plus connu de Faerûn</h1>
     </header>
   );
