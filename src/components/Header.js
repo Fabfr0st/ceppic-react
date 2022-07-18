4dbe986d0d0870f90acb7ae9431aa689798c7761
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-07-18 14:00:12 +0200

    déstructuration

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import "./Header.css"
 
-export default function Header(props) {
+export default function Header({logo}) {
   const minscDesc = "Rôdeur le plus connu de Faerûn";
 
   return (
     <header>
-      <img src={props.logo} alt={minscDesc}></img>
+      <img src={logo} alt={minscDesc}></img>
       <h1>Rôdeur le plus connu de Faerûn</h1>
     </header>
   );
