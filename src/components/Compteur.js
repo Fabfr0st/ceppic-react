ab5af71c54c5ec0ff9207dec9ad578a6451f9710
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-07-22 10:00:53 +0200

    style

--- src/components/Compteur.js
+++ src/components/Compteur.js
@@ -1,13 +1,21 @@
 import { useState } from "react";
 
-export default function Compteur({increment}) {
+export default function Compteur({ increment }) {
   const [count, setCount] = useState(0);
 
   return (
-    <section id="compteur">
-          <p>Vous avez appuyé sur le bouton ci dessous {count} fois</p>
-          {count >= 50 ? <p>Ah oui quand même, Minsc est impressioné !</p> : <p></p>}
-      <button onClick={() => setCount(count + increment)}>Incrémenter</button>
+    <section className="compteur">
+      <div>
+        <p>Vous avez appuyé sur le bouton ci dessous {count} fois</p>
+        {count >= 50 ? (
+          <p>Ah oui quand même, Minsc est impressioné !</p>
+        ) : (
+          <p></p>
+        )}
+      </div>
+      <div>
+        <button onClick={() => setCount(count + increment)}>Incrémenter</button>
+      </div>
     </section>
   );
 }
