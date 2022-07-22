179150e611287b80e9a5ff7e04e6dd549174bb6a
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-07-22 11:52:46 +0200

    compteur v2 et citation

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,6 +4,7 @@ import Message from "./Message";
 import Calcul from "./Calcul";
 import Connexion from "./Connexion";
 import Voiture from "./Voiture";
+import Citation from "./Citations";
 
 export default function Content() {
   return (
@@ -12,8 +13,19 @@ export default function Content() {
       <Calcul />
       <Connexion />
       <Voiture />
-      <Compteur increment={1} />
-      <Compteur increment={10} />
+      <Compteur
+        increment={1}
+        starter={0}
+        text={'"Prêt à l\'attaque, armure et épée en place."'}
+      />
+      <Compteur
+        increment={10}
+        starter={7}
+        text={
+          "\"What? Boo is outraged! See his fury! It's small, so look close. Trust me, it's there.\""
+        }
+      />
+      <Citation />
     </section>
   );
 }
