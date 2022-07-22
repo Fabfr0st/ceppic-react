10b1514e0b79e3d1e7542ed883f8508fcea16afe
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-07-22 10:14:46 +0200

    header en styled-component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
-import "./Header.css"
+// import "./Header.css"
+import styled from "styled-components";
 
-export default function Header({logo}) {
+export default function Header({ logo }) {
   const MinscDesc = "Rôdeur le plus connu de Faerûn";
 
+  const Title = styled.h1`
+    margin: 1rem;
+  `;
+
+  const Logo = styled.img`
+    margin: 1rem;
+  `;
+
+  const Entete = styled.header`
+    background-color: rgba(56, 62, 68, 0.659);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  `;
+
   return (
-    <header>
-      <img src={logo} alt={MinscDesc}></img>
-      <h1>Rôdeur le plus connu de Faerûn</h1>
-    </header>
+    <Entete>
+      <Logo src={logo} alt={MinscDesc}></Logo>
+      <Title>Rôdeur le plus connu de Faerûn</Title>
+    </Entete>
   );
 }
