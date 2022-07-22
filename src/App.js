00b3dea325052ec6491ca8bcb5ebbe44293471b7
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-07-22 16:56:03 +0200

    mise en place routeur

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import minsc from "./Minsc.jpg";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <Header logo={minsc} />
       <main>
         <Content />
