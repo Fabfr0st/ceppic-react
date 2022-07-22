73b7e7fef0e6d05c79618981b2950bafb4718fdc
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-07-22 15:03:56 +0200

    clé des components

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,8 +4,8 @@ import UserCard from "./UserCard.js";
 export default function Users() {
   return (
     <section id="utilisateurs">
-      {userList.map((utilisateur) => (
-        <UserCard utilisateur={utilisateur} />
+          {userList.map((utilisateur) => (
+              <UserCard key={utilisateur.userId} utilisateur={utilisateur} />
       ))}
     </section>
   );
