a051ff27f885e00ab1891f2339a053c2b16df256
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-08-16 11:04:51 +0200

    Page 404

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import minsc from "./Minsc.jpg";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
